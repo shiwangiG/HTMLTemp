1faabb8a88a6130321d7e167cb86b6b444cc5812
shiwangiG <48514428+shiwangiG@users.noreply.github.com>
2019-11-01 07:15:04 +0530

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,10 @@
+
+	function myFunction() {
+    
+
+var x = document.getElementById("demo");
+var createform = document.createElement('form'); // Create New Element Form
+x.appendChild(createform);
+
+
+} 
